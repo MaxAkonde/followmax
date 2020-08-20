adfae9a2177595d4f4492005796237ddb0ca77fe
MaxAkonde <devdevmaster@gmail.com>
2020-08-20 17:16:25 +0200

    add js

--- functions.php
+++ functions.php
@@ -9,4 +9,13 @@ function followmax_register_styles() {
 
 add_action('wp_enqueue_scripts', 'followmax_register_styles');
 
+function followmax_register_scripts() {
+    wp_enqueue_script( 'followmax-jquery', get_template_directory_uri() . "/assets/js/jquery-slim.min.js", array(), '3.2.1', true);
+    wp_enqueue_script( 'followmax-popper', get_template_directory_uri() . "/assets/js/popper.min.js", array(), '1.0', true);
+    wp_enqueue_script( 'followmax-bootstrap', get_template_directory_uri() . "/assets/js/bootstrap.min.js", array(), '4.0', true);
+    wp_enqueue_script( 'followmax-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+}
+
+add_action( 'wp_enqueue_scripts', 'followmax_register_scripts');
+
 ?>
