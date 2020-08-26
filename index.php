32341e6157196a205c4bca9b83fd3511377e4a14
MaxAkonde <devdevmaster@gmail.com>
2020-08-26 17:45:24 +0200

    set content template

--- index.php
+++ index.php
@@ -4,21 +4,23 @@
 
 ?>
 
-<article class="content px-3 py-5 p-md-5">
-	<?php
-		if(have_posts()) {
-			while (have_posts()) {
-				the_post();
-                //the_content();
-                get_template_part('templates-parts/content', 'archive');
+<section class="content px-3 py-5 p-md-5">
+    <div class="container">
+		<?php
+			if(have_posts()) {
+				while (have_posts()) {
+					the_post();
+					//the_content();
+					get_template_part('templates-parts/content', 'archive');
+				}
 			}
-		}
-    ?>
-    
-    <?php
-        the_posts_pagination();
-    ?>
-</article>
+		?>
+		
+		<?php
+			the_posts_pagination();
+		?>
+    </div>
+</section>
 
 <?php
 	get_footer();
--- templates-parts/content-article.php
+++ templates-parts/content-article.php
@@ -3,7 +3,7 @@
         <div class="meta mb-3">
             <span class="date"><?php the_date(); ?></span>
             <?php
-                the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
+            the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
             ?>
             <span class="tag"><i class='fa fa-tag'></i> category</span>
             <span class="comment">
@@ -13,11 +13,14 @@
     </header>
 
     <div class="content-body">
+        <figure class="blog-banner">
+            <a href="#"><img class="img-fluid" src="<?php the_post_thumbnail_url('post-large'); ?>" alt="image"></a>
+        </figure>
         <?php
-            the_content();
+        the_content();
         ?>
     </div>
     <?php
-        comments_template();
+    comments_template();
     ?>
 </div>
